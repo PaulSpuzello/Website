ec4788b1925459fd47928d6a99ef5c0cf00338bd
PaulSpuzello <paspuzello@dmacc.edu>
2019-09-22 15:47:31 -0500

    Assignment 5.1 (not finished)

---
+++ Assignment_5_1_StarterCode/CIS174_TestCoreApp/Models/CreatePersonViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CIS174_TestCoreApp.Models
+{
+    public class CreatePersonViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(25)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Middle Name")]
+        [StringLength(25)]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(25)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Range(1, 120)]
+        public int Age { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm Email")]
+        [Compare("Email")]
+        public string ConfirmEmail { get; set; }
+
+        [Url]
+        [MinLength(7)]
+        public string Website { get; set; }
+
+        [ReadOnly(true)]
+        public string School { get; set; }
+    }
+}
