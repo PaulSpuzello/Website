439f9f12d9a6322c97a6c32ae605d803e586ff30
PaulSpuzello <paspuzello@dmacc.edu>
2019-10-16 21:30:03 -0500

    Assignment 8.1

---
+++ Assignment_5_1_StarterCode/CIS174_TestCoreApp/Controllers/HouseViewModelController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS174_TestCoreApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HouseController : Controller
+    {
+        List<String> _houses = new List<string>
+        {
+            "4",
+            "1854",
+            "05/28/1973",
+            "Carpet"
+        };
+
+        List<String> _houses2 = new List<string>
+        {
+            "3",
+            "1675",
+            "10/17/2015",
+            "Hardwood"
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Index()
+        {
+            return _houses;
+        }
+
+        [HttpGet("{Bedrooms}")]
+        public ActionResult<int> Get(int Bedrooms)
+        {
+            return Json(Ok());
+        }
+
+        [HttpGet("{SqaureFeet}")]
+        public ActionResult<int> get(int SqaureFeet)
+        {
+            return Ok();
+
+            //return notfound();
+        }
+
+        [HttpPost("{HouseViewModel}")]
+        public ActionResult<int> Post(int HouseViewModel)
+        {
+            return Json(StatusCode(201));
+        }
+    }
+
+    public class ValueModel
+    {
+    }
+}
+
---
+++ Assignment_5_1_StarterCode/CIS174_TestCoreApp/Models/HouseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CIS174_TestCoreApp.Models;
+
+namespace DemoWebApi.Models
+{
+    public class HouseViewModel
+    {
+        public string Bedrooms { get; set; }
+        public string SquareFeet { get; set; }
+        public string DateBuilt { get; set; }
+        public string Flooring { get; set; }
+
+    }
+}
